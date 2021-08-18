8697ceb807252f83afdf5c1246e3029637630f49
TayoAderiye <tayoriye@gmail.com>
2021-08-18 21:02:36 +0100

    first commit

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>()
+  term = ''
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onInput(event: any){
+    const target = event.target as HTMLInputElement;
+    this.term = target.value
+
+  }
+  onFormSubmit(event : any){
+    event.preventDefault()
+    this.submitted.emit(this.term)
+    //console.log(this.term)
+  }
+
+}
